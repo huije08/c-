7a4223a25b68af1333514ced742a3b554267e14d
huije08 <huijegang@gmail.com>
2025-02-04 19:45:18 +0900

    (2025.2.4) 예외 처리

---
+++ Class10th (template)/Source.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class List
+{
+public:
+	int size;
+	int index;
+
+	T* pointer;
+
+	List()
+	{
+		size = 0;
+		index = 0;
+		pointer = nullptr;
+	}
+
+	void Resize(int size)
+	{
+		this->size = size;
+
+		pointer = new T[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			pointer[i] = 0;
+		}
+	}
+
+	void Add(T data)
+	{
+		if (index >= size)
+		{
+			cout << "Index Out of Range" << endl;
+		}
+		else
+		{
+			pointer[index++] = data;
+		}
+	}
+
+	const T& operator [] (int index)
+	{
+		return pointer[index];
+	}
+
+	~List()
+	{
+		if (pointer != nullptr)
+		{
+			delete pointer;
+		}
+	}
+
+};
+
+template <typename T>
+bool Same(T argument, T parameter)
+{
+	return argument == parameter;
+}
+
+template<>
+bool Same(const char* argument, const char* prameter)
+{
+	cout << "template Specialization" << endl;
+
+	return strlen(argument) == strlen(parameter);
+}
+
+
+int main()
+{
+#pragma region 템플릿
+	// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터
+	// 형식을 가질 수 있는 기술에 초점을 두어 재사용성을 높일 수 
+	// 있는 기능입니다.
+
+	//List<int> list;
+	//
+	//list.Resize(5);
+	//
+	//list.Add(1);
+	//list.Add(2);
+	//list.Add(3);
+	//list.Add(4);
+	//list.Add(5);
+	//list.Add(6);
+	//
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	cout << list[i] << endl;
+	//}
+
+#pragma endregion
+
+#pragma region 템플릿의 특수화
+	//특정 자료형에 대해 다르게 처리하고 싶을 경우 
+	//특정한 자료형만 다른 형식으로 동작시키는 템플릿 기능입니다.
+
+	//cout << Same('A', 'B') << endl;
+	//cout << Same(15.f, 12.5f) << endl;
+	//cout << Same(10, 10) << endl;
+	//cout << Same("Apple", "Apple") << endl;
+
+
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,106 +2,102 @@
 
 using namespace std;
 
-template <typename T>
-class List
+class Packet
 {
+private:
+	int errorCode = -9999;
+
 public:
-	int size;
-	int index;
-
-	T* pointer;
-
-	List()
+	Packet()
 	{
-		size = 0;
-		index = 0;
-		pointer = nullptr;
+		cout << "Created Packet" << endl;
 	}
 
-	void Resize(int size)
+	const int & ErrorCode()
 	{
-		this->size = size;
-
-		pointer = new T[size];
-
-		for (int i = 0; i < size; i++)
-		{
-			pointer[i] = 0;
-		}
+		return errorCode;
+	}
+	~Packet()
+	{
+		cout << "Release Packet" << endl;
 	}
 
-	void Add(T data)
-	{
-		if (index >= size)
-		{
-			cout << "Index Out of Range" << endl;
-		}
-		else
-		{
-			pointer[index++] = data;
-		}
-	}
-		
-	const T& operator [] (int index)
-	{
-		return pointer[index];
-	}
-
-	~List()
-	{
-		if (pointer != nullptr)
-		{
-			delete pointer;
-		}
-	}
 
 };
 
-template <typename T>
-bool Same(T argument, T parameter)
+class Resource
 {
-	return argument == parameter;
+public:
+	Resource() 
+	{
+		cout << "Created" << endl;
+	}
+
+	~Resource()
+	{
+		cout << "Created" << endl;
+	}
+
+private:
+
+};
+
+Resource::Resource()
+{
 }
 
-
+Resource::~Resource()
+{
+}
 
 int main()
 {
-#pragma region 템플릿
-	// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터
-	// 형식을 가질 수 있는 기술에 초점을 두어 재사용성을 높일 수 
-	// 있는 기능입니다.
+#pragma region 스마트 포인터
+	//포인터를 사용하는 동시에 메모리를 자동으로 메모리 관리를
+	// 해주며, 경계 확인과 같은 추가 기능을 제공하는 추상
+	// 데이터 포인터 형식입니다.
 
-	//List<int> list;
-	//
-	//list.Resize(5);
-	//
-	//list.Add(1);
-	//list.Add(2);
-	//list.Add(3);
-	//list.Add(4);
-	//list.Add(5);
-	//list.Add(6);
-	//
-	//for (int i = 0; i < 5; i++)
-	//{
-	//	cout << list[i] << endl;
-	//}
+#pragma region unique pointer
+	// 특정한 객체를 하나의 스마트 포인터만 가리킬 수 
+	// 있도록 되어있는 포인터입니다.
+
+	unique_ptr<Packet> uniquePointer = make_unique<Packet>();
+
+	cout << "uniquePointer Error Code : " << uniquePointer->ErrorCode() << endl;
 
 #pragma endregion
 
-#pragma region 템플릿의 특수화
-	//특정 자료형에 대해 다르게 처리하고 싶을 경우 
-	//특정한 자료형만 다른 형식으로 동작시키는 템플릿 기능입니다.
+#pragma region shared pointer
+	// 하나의 자원 객체를 여러 포인터 객체가 가리킬 수 있으며,
+	// 모든 포인터 객체가 지원 객체를 필요하지 않을때 자원 객체를
+	// 해제하도록 설계되어 있는 포인터 입니다.
 
-	cout << Same('A', 'B') << endl;
-	cout << Same(15.f, 12.5f) << endl;
-	cout << Same(10, 10) << endl;
-	cout << Same("Apple", "Apple") << endl;
-	
+	shared_ptr<Resource> oil = make_shared<Resource>();
 
+	cout << "Reference Count : " << oil.use_count() << endl;
+
+	shared_ptr<Resource> mineral = oil;
+
+	cout << "Reference Count : " << oil.use_count() << endl;
 
 #pragma endregion
+
+
+
+	int* pointer2 = nullptr;
+
+	{
+		int * pointer1 = new int;
+
+		*pointer1 = 100;
+
+		pointer2 = pointer1;
+	}
+
+	cout << "pointer가 가리키는 값:" << *pointer2 << endl;
+
+#pragma endregion
+
 
 	return 0;
 }
