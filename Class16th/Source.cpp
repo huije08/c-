2336073c11533cfd284cecd15e77d8b052c4b552
huije08 <huijegang@gmail.com>
2025-02-10 19:44:38 +0900

    (2025.2.10) 연관 컨테이너

---
+++ Class16th/Source.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+#pragma region Iterator
+
+	//std::vector<int> vector;
+	//
+	//std::vector<int>::iterator iterator;
+	//
+	//vector.push_back(10);
+	//vector.push_back(20);
+	//vector.push_back(30);
+	//vector.push_back(40);
+	//vector.push_back(50);
+	//
+	//
+	//
+	//for (iterator = vector.begin(); iterator != vector.end(); iterator++)
+	//{
+	//	cout << *iterator << " ";
+	//
+	//}
+
+#pragma endregion
+
+#pragma region set
+
+	//std::set<int> set;
+	//
+	//set.insert(10);
+	//set.insert(20);
+	//set.insert(30);
+	//set.insert(40);
+	//set.insert(50);
+	//
+	//if (set.find(10) != set.end())
+	//{
+	//	cout << "Find" << endl;
+	//}
+	//else
+	//{
+	//	cout << "Not Ket Found" << endl;
+	//}
+	//
+	//set.erase(40);
+	//
+	//for (std::set<int>::iterator iterator = set. begin(); iterator != set.end(); iterator++)
+	//{
+	//	
+	//}
+#pragma endregion
+
+#pragma region map
+
+	std::map<string, int> map;
+
+	map.insert({ "Sword", 1000 });
+	map.insert({ "Armor", 2000 });
+
+	if (map.find("Shose") != map.end())
+	{
+		cout << "Find" << endl;
+	}
+	else
+	{
+		cout << "Not Key Found" << endl;
+	}
+
+	map.erase("Sword");
+
+	for (std::set<string, int>::iterator iterator = map.begin(); iterator != map.end(); iterator++)
+	{
+		cout << iterator->first << endl;
+		cout << iterator->second << endl;
+	}
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,25 +1,9 @@
 ﻿#include <iostream>
-#include <stack>
 
 using namespace std;
 
 int main()
 {
-#pragma region Stack
-	std::stack<int> stack;
-
-	stack.push(10);
-	stack.push(20);
-	stack.push(30);
-
-	while (stack.empty() == false)
-	{
-		cout << stack.top() << endl;
-
-		stack.pop();
-	}
-
-#pragma endregion
 
 	return 0;
 }
